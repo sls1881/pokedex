c57d29976f9622f5e844c0764430247a4c37c799
‘Shelby <sls1881@gmail.com>
2021-02-12 18:59:16 -0800

    CSS

--- src/Search/PokeItem.js
+++ src/Search/PokeItem.js
@@ -15,7 +15,7 @@ export default class PokeItem extends Component {
         } = this.props;
 
         return (<li key={url_image}>
-            <img alt='pokemon' src={url_image} />
+            <img className='image-items' alt='pokemon' src={url_image} />
             <h3>Pokemon name: {pokemon}</h3>
             <p>Attack: {attack}</p>
             <p>Defense: {defense}</p>
